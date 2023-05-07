75342ea83caf28bbdbe9e10e475e677842e252a9
Billy Rigdon <brigdon@rmsweb.com>
2023-05-07 15:06:01 -0500

    route fix, will come back later

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,5 +8,5 @@ cd ../Backend
 go get
 go build .
 cd ..
-docker build -t libretrac/libretrac:2.1.4 .
-docker push libretrac/libretrac:2.1.4
+docker build -t libretrac/libretrac:2.1.5 .
+docker push libretrac/libretrac:2.1.5
